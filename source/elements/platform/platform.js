0b7f599e6178d3adb6bef52d18541af4d9c64b51
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2016-07-20 18:44:27 +0300

    add mouse rotate

---
+++ source/elements/platform/platform.js
@@ -0,0 +1,14 @@
+'use strict';
+window.onload = function () {
+	var platform = document.getElementById('platform')
+		, platformOuter = document.getElementById('platform_outer');
+
+
+		new mouseMoveParralax({
+			container: platform
+			, element: platformOuter
+			, braking: 5
+			, isRotate: true
+			, isOposite: true
+		});
+};
