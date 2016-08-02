e3067b5b7230315138eb98f46d234a200cc12f5b
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2016-08-02 19:16:22 +0300

    add some actions to buttons

--- source/elements/controls/controls.js
+++ source/elements/controls/controls.js
@@ -1,13 +1,14 @@
 'use strict';
 $(document).ready(function() {
 	var $showPokemonButton = $('#show_pokemon')
-		, show3dButton = $('#show_3d')
+		, $show3dButton = $('#show_3d')
 		, $movePlatformButton = $('#move_platform')
 		, buttonActiveClass = 'active'
 		, $hero = $('#hero')
 		, showHeroClass = 'hero-is-hide'
 		, $platform = $('#platform')
-		, platformMoveClass = 'platform-move';
+		, platformMoveClass = 'platform-move'
+		, platform3dClass = 'platform-3d';
 
 		var activeOption = function (button, element, elmentActiveClass) {
 			button.on('click', function(event) {
@@ -20,4 +21,5 @@ $(document).ready(function() {
 
 		activeOption($showPokemonButton, $hero, showHeroClass);
 		activeOption($movePlatformButton, $platform, platformMoveClass);
+		activeOption($show3dButton, $platform, platform3dClass);
 });
